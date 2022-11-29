77f2552024c01f213573cade3267e68c4557b49e
Lohuama Lima <limalorruama98@gmail.com>
2022-11-28 21:55:30 -0300

    'u'

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,55 +1,51 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = 'icon';
 
-let techs = [
-    'bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'
-];
-
-let cards = null;
 
 startGame();
 
 function startGame(){
-    cards = createCardsFromTechs(techs);
+    //game.createCardsFromTechs();
+    initiaLizeCards(game.createCardsFromTechs());
 }
 
-function shuffleCards(cards){
+function initiaLizeCards(cards){
+    let gameBoard = document.getElementById('gameBoard');
     
+    cards.forEach(card => {
+
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard)
+        gameBoard.appendChild(cardElement);
+    })
 }
 
-createCardsFromTechs(techs);
-function createCardsFromTechs(techs){
-
-    let cards = [];
-
-    for(let tech of techs){
-        cards.push(createPairFromTech(tech));
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+function createCardFace(face, card, element){
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src="./assets/images/" + card.icon + ".png"
+        cardElementFace.appendChild(iconElement)
+    }else{
+        cardElementFace.innerHTML = '&lt/&gt';
     }
-
-    return cards.flatMap(pair => pair);
+    element.appendChild(cardElementFace);
 }
 
-function createPairFromTech(tech){
-    return [{
-        id: createIdWidthTech(tech),
-        icon: tech,
-        flipped: false,
-    },{
-        id: createIdWidthTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
-
-function createIdWidthTech(tech){
-    return tech + parseInt(Math.random() * 1000);
+function flipCard(){
+    this.classList.add('flip')
 }
